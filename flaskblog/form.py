1adfc645216db63e778fcfec78bd2184840200d3
real-music <fedjioraymond@gmail.com>
2019-10-06 23:53:26 +0100

    User can now update account and profile picture

--- flaskblog/form.py
+++ flaskblog/form.py
@@ -1,5 +1,7 @@
 from flaskblog.models import User, Post
+from flask_login import current_user
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -49,3 +51,28 @@ class LoginForm(FlaskForm):
 
     # Submit Button
     submit = SubmitField('Log in')
+
+
+class UpdateAccountForm(FlaskForm):
+    # username Validation
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    # Email Validation
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    # Submit Button
+    submit = SubmitField('Update')
+
+    # Check if username already exist
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    'Username already exist. Choose another name.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email already exist.')
