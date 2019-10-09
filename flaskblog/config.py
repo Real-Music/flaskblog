d367179ebbf72dc42bfb0c6b24ddf7525999ba8c
real-music <fedjioraymond@gmail.com>
2019-10-09 14:18:41 +0100

    Final Commit, Embeded Chart in the About Route

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '37e88810d98a26cfbef850f976986f74'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://raymond:12345@localhost/flaskblog' or 'postgresql://raymond:12345@127.0.0.1/flaskblog'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_POST = 587
     MAIL_USE_TLS = True
